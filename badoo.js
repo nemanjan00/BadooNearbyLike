edbca106419b52dac7f7cf0968cd4f93bb9c027e
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-06-18 12:05:57 +0200

    English

--- badoo.js
+++ badoo.js
@@ -102,7 +102,7 @@ window.bot = {
 
 	massLike: function(){
 		bot.getUsers().then(function(data){
-			users = data.filter(function(user){return user.online_status_text != "online_status_text" && user.online_status_text != "Bila je na mreži pre više od nedelju dana" && user.online_status_text != " Was online more than a week ago "});
+			users = data.filter(function(user){return user.online_status_text != "online_status_text" && user.online_status_text != "Bila je na mreži pre više od nedelju dana" && user.online_status_text != " Was online more than a week ago"});
 
 			users = users.map(function(user){return user.user_id});
 
--- userscript.js
+++ userscript.js
@@ -8,7 +8,7 @@
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/BadooNearbyLike/master/badoo.user.js
 // @namespace https://github.com/nemanjan00/BadooNearbyAutolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/BadooNearbyLike/master/badoo.user.js
-// @version 4
+// @version 5
 // ==/UserScript==
 
 var code = atob("{{code}}");
