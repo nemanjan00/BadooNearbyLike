5795d16efcc02eda3c4d3b1010ae4f5d2d496c6d
José Júnior <jjnilton@users.noreply.github.com>
2017-11-16 14:44:48 -0300

    Update badoo.user.js
    include blendr.com in supported sites

--- badoo.user.js
+++ badoo.user.js
@@ -5,6 +5,10 @@
 // @include https://*.badoo.com/*
 // @include http://badoo.com/*
 // @include http://*.badoo.com/*
+// @include https://blendr.com/*
+// @include https://*.blendr.com/*
+// @include http://blendr.com/*
+// @include http://*.blendr.com/*
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/BadooNearbyLike/master/badoo.user.js
 // @namespace https://github.com/nemanjan00/BadooNearbyAutolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/BadooNearbyLike/master/badoo.user.js
